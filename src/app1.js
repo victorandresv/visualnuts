bdcbe16e7433536628a1d53a5f23742339eb7851
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2022-07-12 09:04:56 -0300

    upgrade exercise 1

--- src/app1.js
+++ src/app1.js
@@ -1,11 +1,46 @@
-for(n=1;n<=100;n++){
-    if(n%3 == 0 && n%5 == 0){
-        console.log('Visual Nuts')
-    } else if(n%3 == 0){
-        console.log('Visual')
-    } else if(n%5 == 0){
-        console.log('Nuts')
-    } else {
-        console.log(n)
+
+/**
+ * Get the whole integer numbers to the console, start 
+ * from the number 1, and get all numbers going up to the number 100,
+ * showing string messages when is divisible by 3 and 5
+ * @returns array
+ */
+function numbers(){
+    const numbers = [];
+    for(n=1;n<=100;n++){
+        if(n%3 == 0 && n%5 == 0){
+            numbers.push({
+                number: n,
+                label: 'Visual Nuts'
+            })
+        } else if(n%3 == 0){
+            numbers.push({
+                number: n,
+                label: 'Visual'
+            })
+        } else if(n%5 == 0){
+            numbers.push({
+                number: n,
+                label: 'Nuts'
+            })
+        } else {
+            numbers.push({
+                number: n,
+                label: ''
+            })
+        }
     }
+    return numbers;
 }
+
+/**
+ * Print exercise
+ */
+const data = numbers()
+data.forEach(item => {
+    if(item.label == ''){
+        console.log(item.number)
+    } else {
+        console.log(item.label)
+    }
+})
